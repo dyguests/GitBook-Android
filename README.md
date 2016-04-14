5576b368e703edb8d262dfaab9eaeebe107907ab
fanhl <Dyguest0315>
2016-04-14 23:17:36 +0800

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# GitBook-Android
+
+Coding with Kotlin,dagger2,RxJava,RxAndroid,Retrofit,Picasso,MVP,Material Design
+
+[GitBook 简明教程](http://www.chengweiyang.cn/gitbook/)
+
+[GIT教學](https://www.gitbook.com/book/kingofamani/git-teach/details)
--- app/src/main/kotlin/com/fanhl/gitbook/ui/MainActivity.kt
+++ app/src/main/kotlin/com/fanhl/gitbook/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+    val TAG = MainActivity::class.java.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
