824bfb00ec76ffe473c1da9621289720acf0567f
fanhl <Dyguest0315>
2016-04-19 09:10:48 +0800

    ic_launcher

---
+++ app/src/main/kotlin/com/fanhl/gitbook/ui/MineFragment.kt
@@ -0,0 +1,17 @@
+package com.fanhl.gitbook.ui
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fanhl.gitbook.R
+
+/**
+ * Created by fanhl on 16/4/19.
+ */
+class MineFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_mine, container, false)
+    }
+}
